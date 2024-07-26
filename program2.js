a037aa4dd382ed0a7a51e2888200ffe30081e640
anjali-tomar555 <143319849+anjali-tomar555@users.noreply.github.com>
2024-07-26 13:27:24 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,35 @@
-const decodeTheRing = function (s, p) {
-
-    // write your code here
-
+  const decodeTheRing = function (s, p) {
+    let sIndex = 0, pIndex = 0;
+    let starIndex = -1, matchIndex = 0;
+  
+    while (sIndex < s.length) {
+      if (pIndex < p.length && (p[pIndex] === '?' || p[pIndex] === s[sIndex])) {
+        // Current characters match, or pattern has '?' which can match any single character
+        sIndex++;
+        pIndex++;
+      } else if (pIndex < p.length && p[pIndex] === '*') {
+        // Found a '*', mark the position and proceed
+        starIndex = pIndex;
+        matchIndex = sIndex;
+        pIndex++;
+      } else if (starIndex !== -1) {
+        // No match, but there was a previous '*', try to extend its match
+        pIndex = starIndex + 1;
+        matchIndex++;
+        sIndex = matchIndex;
+      } else {
+        // No match and no previous '*', fail the match
+        return false;
+      }
+    }
+  
+    // Check if remaining characters in the pattern can be accounted for
+    while (pIndex < p.length && p[pIndex] === '*') {
+      pIndex++;
+    }
+  
+    return pIndex === p.length;
   };
   
   module.exports = decodeTheRing;
+  
